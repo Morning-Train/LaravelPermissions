39e3aeb2c3650a98bc67ae2ab4f45902bee0c889
Mateusz Grochala <mateusz.grochala@yahoo.dk>
2019-04-02 11:40:31 +0200

    don't need to check if class exists, since we require the package

--- database/seeds/PermissionSeeder.php
+++ database/seeds/PermissionSeeder.php
@@ -4,6 +4,8 @@ namespace MorningTrain\Laravel\Resources\Database\Seeds;
 
 use Illuminate\Database\Seeder;
 use MorningTrain\Laravel\Resources\ResourceRepository;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
 
 class PermissionSeeder extends Seeder
 {
@@ -14,15 +16,11 @@ class PermissionSeeder extends Seeder
 
     protected function seedPermissions(array $permissions)
     {
-        // We assume this means the package is installed
-        if (class_exists('\Spatie\Permission\PermissionRegistrar')) {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
-            // Reset cached roles and permissions
-            app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
-
-            foreach ($permissions as $permission) {
-                \Spatie\Permission\Models\Permission::create(['name' => $permission]);
-            }
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
         }
     }
 }

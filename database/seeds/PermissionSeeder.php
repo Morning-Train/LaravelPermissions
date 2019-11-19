e50fe02f791fcc943b0205770c5e23533dba26a5
Mateusz Grochala <mateusz.grochala@yahoo.dk>
2019-11-19 08:33:36 +0100

    bugfix: now also seeds custom permissions

--- database/seeds/PermissionSeeder.php
+++ database/seeds/PermissionSeeder.php
@@ -3,17 +3,27 @@
 namespace MorningTrain\Laravel\Permissions\Database\Seeds;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 use MorningTrain\Laravel\Permissions\Permissions;
-use MorningTrain\Laravel\Resources\ResourceRepository;
 use Spatie\Permission\Models\Permission;
 
 class PermissionSeeder extends Seeder
 {
     public function run()
     {
-        foreach (Permissions::getRestrictedOperationIdentifiers() as $name) {
+        $permissions = array_unique(array_merge(
+            array_keys(config('permissions.custom_permission_roles', [])),
+            Permissions::getRestrictedOperationIdentifiers()
+        ));
+
+        $permissionRoles = array_merge_recursive(
+            config('permissions.permission_roles', []),
+            config('permissions.custom_permission_roles', [])
+        );
+
+        foreach ($permissions as $name) {
             $permission = Permission::create(['name' => $name]);
-            $roles      = config('permissions.permission_roles.'.$name, []) ?? [];
+            $roles      = Arr::get($permissionRoles, $permission->name, []);
 
             if (!empty($roles)) {
                 $permission->syncRoles($roles);
